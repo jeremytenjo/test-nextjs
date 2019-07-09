798970ed581549916cce05a40521bfbc0f0b0dca
Jeremy Tenjo <jeremy.tenjo.fcvinteractive.com>
2019-07-09 15:35:26 -0700

    Addd pwa

--- constants/head.js
+++ constants/head.js
@@ -7,6 +7,9 @@ export default () => (
         href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
         rel="stylesheet"
       />
+      <title>TV Guide</title>
+      <link rel="manifest" href="./static/manifest.json" />
+      <link rel="icon" type="image/png" href="./static/logo.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
