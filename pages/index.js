313db1f93bdd2dde6ccfd5e6fc32ca183cd5718c
Jeremy Tenjo <jeremy.tenjo.fcvinteractive.com>
2019-07-09 10:19:32 -0700

    f

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import fetch from 'isomorphic-unfetch'
 import Router from 'next/router'
 import baserUrl from '../constants/baseUrl'
 
-import Poster from '@tenjojeremy/web-toolkit/build/Data-Display/Cards/Poster/Ui/React/Styles/1/poster.1.index.js'
-import List from '@tenjojeremy/web-toolkit/build/Data-Display/List/Ui/React/list.index.js'
+// import Poster from '@tenjojeremy/web-toolkit/build/Data-Display/Cards/Poster/Ui/React/Styles/1/poster.1.index.js'
+// import List from '@tenjojeremy/web-toolkit/build/Data-Display/List/Ui/React/list.index.js'
 
 const image =
   'https://images-na.ssl-images-amazon.com/images/I/818NtgncwLL._SL1500_.jpg'
@@ -16,11 +16,15 @@ const Index = ({ movies }) => {
 
   return (
     <div>
-      <List photoGrid onItemClick={(e) => console.log(e)}>
+      {/* <List photoGrid onItemClick={(e) => console.log(e)}>
         {movies.map(({ posterUrl, ...rest }) => {
           return <Poster key={posterUrl} src={posterUrl} {...rest} />
         })}
-      </List>
+      </List> */}
+
+      {movies.map(({ posterUrl, ...rest }) => {
+        return <img key={posterUrl} src={posterUrl} />
+      })}
     </div>
   )
 }
